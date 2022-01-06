5a67c2d4f0fddad05e1672f6743270060e2a88a5
Ercan Yildiz <ercan.yildiz@inventure.de>
2022-01-06 19:30:00 +0100

    client iterating 10 rounds and fetching data from user input, validating that only numeric values are passed

--- src/main/kotlin/com/yildiz/bowlinggame/GameRunner.kt
+++ src/main/kotlin/com/yildiz/bowlinggame/GameRunner.kt
@@ -1,6 +1,8 @@
 package com.yildiz.bowlinggame
 
+import com.yildiz.bowlinggame.dto.FrameDTO
 import com.yildiz.bowlinggame.dto.PlayerDTO
+import com.yildiz.bowlinggame.model.Frame
 import com.yildiz.bowlinggame.service.FrameService
 import com.yildiz.bowlinggame.service.GameService
 import com.yildiz.bowlinggame.service.PlayerService
@@ -8,6 +10,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import java.lang.NumberFormatException
 import javax.transaction.Transactional
 
 /*
@@ -32,10 +35,34 @@ class GameRunner @Autowired constructor(
         val givenName = readLine()!!
         logger.info("next enter your surname:")
         val surname = readLine()!!
-        logger.info("Your name is $givenName $surname. Start the game and tell us about your roll points. Good luck!")
-
+        logger.info("Your name is $givenName $surname. Bowling Game started good Luck.")
         //save player
         val playerDTO = PlayerDTO(givenName, surname)
         val savedPlayer = playerService.createPlayer(playerDTO)
+        //setup game and define necessary variables
+        val frames: MutableList<Frame> = mutableListOf()
+        val createdGame = gameService.createGame(savedPlayer, frames, 0)
+        var strike = false
+        var spare = false
+        //simulate 10 rounds
+        for(i in 1..10){
+            logger.info("Round[$i] - Please enter your first roll score:")
+            var roundOngoing = true
+            var frame: FrameDTO
+            var firstRoll: String?
+            var sum1: Int
+            var sum2: Int
+            // while round is ongoing we fetch the scores of the rolls from the player
+            while(roundOngoing){
+                try{
+                    firstRoll = readLine()
+                    val firstPoints: Int = Integer.valueOf(firstRoll)
+                    logger.info("First Roll points: $firstPoints")
+                }catch (ex: NumberFormatException){
+                    logger.info("Please type a numeric value!")
+                }
+            }
+        }
     }
+
 }
